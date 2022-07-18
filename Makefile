5b1b44b2f4a749c1ca4d6e24e957ea24c2b5a576
gettsu <tsuge-daisuke430@g.ecc.u-tokyo.ac.jp>
2022-07-18 18:59:11 +0900

    [add] halt

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ qemu: OVMF_CODE.fd OVMF_VARS.fd EFI/BOOT/BOOTX64.EFI
 	qemu-system-x86_64 \
 		-drive if=pflash,format=raw,readonly=on,file=./OVMF_CODE.fd \
 		-drive if=pflash,format=raw,file=./OVMF_VARS.fd \
-		-drive file=fat:rw:.,media=disk,format=raw &
+		-drive file=fat:rw:.,media=disk,format=raw \
+		-monitor stdio
 
 OVMF_VARS.fd:
 	wget https://retrage.github.io/edk2-nightly/bin/RELEASEX64_OVMF_VARS.fd
--- src/main.zig
+++ src/main.zig
@@ -1,44 +1,47 @@
 const std = @import("std");
 const uefi = std.os.uefi;
 const L = std.unicode.utf8ToUtf16LeStringLiteral;
-// const LR = std.unicode.utf8ToUtf16LeWithNull;
-// const fmt = std.fmt;
+const LR = std.unicode.utf8ToUtf16LeWithNull;
+const fmt = std.fmt;
 
-// var memory_map: [4096 * 4]u8 = undefined;
+var memory_map: [4096 * 4]u8 = undefined;
 
 var boot_services: *uefi.tables.BootServices = undefined;
 var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
 var sfsp: ?*uefi.protocols.SimpleFileSystemProtocol = undefined;
 var gop: ?*uefi.protocols.GraphicsOutputProtocol = undefined;
 
-
 var root_dir: *uefi.protocols.FileProtocol = undefined;
 
 pub fn main() void {
     init();
-    while (true) {}
+    halt();
 }
 
 fn init() void {
     boot_services = uefi.system_table.boot_services.?;
     con_out = uefi.system_table.con_out.?;
     _ = con_out.reset(false);
-    
+
     _ = con_out.outputString(L("boot init start\r\n"));
 
     if (boot_services.locateProtocol(&uefi.protocols.SimpleFileSystemProtocol.guid, null, @ptrCast(*?*anyopaque, &sfsp)) != uefi.Status.Success) {
         _ = con_out.outputString(L("cannot locate simple file system protocol\r\n"));
-        while (true) {}
+        halt();
     }
 
     if (sfsp.?.openVolume(&root_dir) != uefi.Status.Success) {
         _ = con_out.outputString(L("cannot open volume\r\n"));
-        while (true) {}
+        halt();
     }
 
     if (boot_services.locateProtocol(&uefi.protocols.GraphicsOutputProtocol.guid, null, @ptrCast(*?*anyopaque, &gop)) != uefi.Status.Success) {
         _ = con_out.outputString(L("cannot locate graphics output protocol\r\n"));
-        while (true) {}
+        halt();
     }
     _ = con_out.outputString(L("boot init success!\r\n"));
+}
+
+fn halt() void {
+    while(true) { asm volatile("hlt"); }
 }
