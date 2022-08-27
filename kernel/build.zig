e8f80fe83dcae922832e2710f4fdb2859f8812c2
gettsu <tsuge-daisuke430@g.ecc.u-tokyo.ac.jp>
2022-08-28 08:13:04 +0900

    [add] font

--- kernel/build.zig
+++ kernel/build.zig
@@ -5,6 +5,7 @@ const CrossTarget = std.zig.CrossTarget;
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("kernel.elf", "src/main.zig");
+    exe.addObjectFile("hankaku.o");
     exe.setTarget(CrossTarget{ .cpu_arch = Target.Cpu.Arch.x86_64, .os_tag = Target.Os.Tag.freestanding, .abi = Target.Abi.gnu });
     exe.setOutputDir("..");
     exe.image_base = 0x100000;
--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -2,6 +2,26 @@ const std = @import("std");
 const uefi = std.os.uefi;
 const GraphicsPixelFormat = uefi.protocols.GraphicsPixelFormat;
 
+extern var _binary_hankaku_bin_start :u8;
+extern var _binary_hankaku_bin_end: u8;
+extern var _binary_hankaku_bin_size: u8;
+
+export fn kernelMain(frame_buffer_config: *FrameBufferConfig) void {
+    var pixel_writer = BGRPixelWriter.new(frame_buffer_config);
+
+    var i: usize = 0;
+    while (i < frame_buffer_config.horizontal_resolution * frame_buffer_config.vertical_resolution) : (i += 1) {
+        pixel_writer.write(i % frame_buffer_config.horizontal_resolution, i / frame_buffer_config.horizontal_resolution, &PixelColor.white);
+    }
+    pixel_writer.writeAscii(0, 0, 'A', &PixelColor.black);
+    pixel_writer.writeAscii(8, 0, 'a', &PixelColor.black);
+    pixel_writer.writeAscii(0, 16, '!', &PixelColor.black);
+    pixel_writer.writeAscii(8, 16, '?', &PixelColor.black);
+    pixel_writer.writeAscii(16, 16, '?', &PixelColor.black);
+    pixel_writer.writeAscii(16, 32, '!', &PixelColor.black);
+    halt();
+}
+
 const FrameBufferConfig = extern struct {
     frame_buffer: [*]u8,
     pixels_per_scan_line: u32,
@@ -14,6 +34,9 @@ const PixelColor = struct {
     r: u8,
     g: u8,
     b: u8,
+    const Self = @This();
+    const white = Self { .r = 255, .g = 255, .b = 255};
+    const black = Self { .r = 0, .g = 0, .b = 0 };
 };
 
 const BGRPixelWriter = struct {
@@ -27,23 +50,38 @@ const BGRPixelWriter = struct {
         var frame_buffer = @ptrCast([*]u8, self.config_.frame_buffer);
         return frame_buffer + 4 * (self.config_.pixels_per_scan_line * y + x);
     }
-    fn write(self: *Self, x: usize, y: usize, col: PixelColor) void {
+    fn write(self: *Self, x: usize, y: usize, col: *const PixelColor) void {
         var p = self.pixel_at(x, y);
         p[0] = col.b;
         p[1] = col.g;
         p[2] = col.r;
     }
+
+    fn getFont(c: u8) ?[*]u8 {
+        var index: usize = 16 * @intCast(usize, c);
+        if (index >= @ptrToInt(&_binary_hankaku_bin_size)) {
+            return null;
+        }
+        return @ptrCast([*]u8, &_binary_hankaku_bin_start) + index;
+    }
+
+    fn writeAscii(self: *Self, x: usize, y: usize, c: u8, col: *const PixelColor) void {
+        const font = Self.getFont(c);
+        if (font == null) {
+            return;
+        }
+        comptime var dy: u8 = 0;
+        inline while (dy < 16) : (dy += 1) {
+            comptime var dx: u8 = 0;
+            inline while (dx < 8) : (dx += 1) {
+                if ((font.?[dy] << dx) & 0x80 != 0) {
+                    self.write(x + dx, y + dy, col);
+                }
+            }
+        }
+    }
 };
 
-export fn kernelMain(frame_buffer_config: *FrameBufferConfig) void {
-    var pixel_writer = BGRPixelWriter.new(frame_buffer_config);
-
-    var i: usize = 0;
-    while (i < frame_buffer_config.horizontal_resolution * frame_buffer_config.vertical_resolution) : (i += 1) {
-        pixel_writer.write(i % frame_buffer_config.horizontal_resolution, i / frame_buffer_config.horizontal_resolution, PixelColor{ .r = 0, .g = 255, .b = 255 });
-    }
-    halt();
-}
 fn halt() void {
     while (true) {
         asm volatile ("hlt");
